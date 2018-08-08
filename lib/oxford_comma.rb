37fccb14bb525712ad5d72274f66e61e6259f903
Mariel Schlaefer <mschlaefer00@gmail.com>
2018-08-08 19:57:35 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,21 @@
 def oxford_comma(array)
-
+  if array.length === 1
+    return array.String
+  end
+  
+  if array.length === 2
+    return array.join(" and ")
+  end
+  
+  if array.length > 2 
+    i = 0 
+    while i < (array.length - 1) do
+      array.join(", ")
+      
+      if i === (array.length - 1)
+        array.join(" and ")
+      end
+    end
+    return array
+  end
 end

11217c9eac2a0098af9ccaee7ef06544bdeca8eb
Mariel Schlaefer <mschlaefer00@gmail.com>
2018-08-08 19:59:54 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,9 @@
 def oxford_comma(array)
-  if array.length == 1
-  return array[0]
-
-else
-  return "#{array[0, array.length].join(', ')} and #{array.last}"
-end
+  if array.length === 1
+    return array[0]
+  else if array.length === 2
+    return array.join(" and ")
+  else
+    return "#{array[0, array.length].join(', ')} and #{array.last}"
+  end
 end
